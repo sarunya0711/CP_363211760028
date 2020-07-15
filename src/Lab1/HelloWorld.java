536e9671d14fe82d27be85f7ebf976109d2af299
sarunya <sarunya.ch@rmutsvmail.com>
2020-07-15 14:58:45 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -6,6 +6,10 @@ public class HelloWorld
     public static void main(String[] args) {
 
     System.out.println("Hello World !!!");
+    System.out.println("sarunya chansangsa ");
+    System.out.println("363211760028 ");
+    System.out.println("เทคโนโลยีการจัดการ ");
+    System.out.println("การจัดการเทคดนโลยีสารสนเทศ ");
 
 
 
