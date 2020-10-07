65b1b3c9100946d69d565f1b53fbf3bd6d95dde8
sarunya <sarunya.ch@rmutsv.ac.th>
2020-10-07 15:33:48 +0700

    sarunya1

---
+++ src/Lab8/Ex_Exception.java
@@ -0,0 +1,24 @@
+package Lab8;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ex_Exception {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Enter an integer number:");
+            int num = sc.nextInt(); //***
+            System.out.println("The number is "+num);
+
+        }catch (InputMismatchException ex){
+               ex.printStackTrace();
+        }
+        System.out.println("Good Bye.");
+
+
+
+
+
+    }
+}
